21000d8c0d03225cd200f191fbe1a289ff2f3f0b
Fadel <59105868+SrWither@users.noreply.github.com>
2022-01-05 21:20:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ update(){
 	cp *.vim ~/.config/nvim
 	mkdir -p ~/.config/nvim/autoload
 	cp autoload/*.vim ~/.config/nvim/autoload
-	nvim -c ":PlugUpgrade | :PlugClean | :PlugUpdate | :qa!"
+	nvim -c ":PlugClean | :PlugUpdate | :qa!"
 	echo "Successful upgrade!"
 }
 
