34a9375bb7e845fe171ace9c49ebc64ddae686b3
SrWither <fjachuf@gmail.com>
2023-02-08 20:51:49 -0300

    improve code

--- init.lua
+++ init.lua
@@ -1,15 +1,17 @@
+vim.cmd("let HOME = '" .. vim.fn.expand('~') .. "'")
+
 require('options')
 require('plugin')
 require('mapping')
-require('colors')
 require('contextmenu')
 
 -- External Config
 vim.cmd("source " .. vim.fn.expand("~/.config/usuim/settings.vim"))
-vim.cmd("source " .. vim.fn.expand("~/.config/usuim/config.vim"))
 vim.cmd("source " .. vim.fn.expand("~/.config/usuim/lsp.vim"))
 vim.cmd("source " .. vim.fn.expand("~/.config/usuim/commands.vim"))
 vim.cmd("source " .. vim.fn.expand("~/.config/usuim/mapping.vim"))
 
 -- Autoinsert
 require('autoinsert')
+-- Colors
+require('colors')
